f17a6cd39d69d463fb69b29dd4ec42e0933951a9
teleyos <tristanduret@hotmail.fr>
2020-10-28 15:37:00 +0100

    push vers heroku

--- contact/index.php
+++ contact/index.php
@@ -1,44 +1,43 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
 
-// Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = '81528f312ff2d2';                     // SMTP username
-    $mail->Password   = '5035f518c6c1e5';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+$mail->isSMTP();
 
-    //Recipients
-    $mail->setFrom('info@mailtrap.io', 'Mailtrap');
-    $mail->addAddress('recipient1@mailtrap.io', 'Tim');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc1@example.com', 'Elena');
-    $mail->addBCC('bcc1@example.com', 'Alex');
+$mail->SMTPAuth = true;
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Test Email via Mailtrap SMTP using PHPMailer';
+$mail->Host = 'smtp.mailtrap.io';
 
-    $mailContent = "<h1>Send HTML Email using SMTP in PHP</h1>
-        <p>This is a test email I’m sending using SMTP mail server with PHPMailer.</p>";
-    $mail->Body = $mailContent;
+$mail->Username = '81528f312ff2d2';
 
-    $mail->send();
+$mail->Password = '5035f518c6c1e5';
+
+$mail->Port = 2525;
+
+$mail->SMTPSecure = 'tls';
+
+$mail->setFrom('info@mailtrap.io', 'Mailtrap');
+$mail->addReplyTo('info@mailtrap.io', 'Mailtrap');
+$mail->addAddress('recipient1@mailtrap.io', 'Tim'); 
+$mail->addCC('cc1@example.com', 'Elena');
+$mail->addBCC('bcc1@example.com', 'Alex');
+
+$mail->Subject = 'Test Email via Mailtrap SMTP using PHPMailer';
+
+$mail->isHTML(true);
+
+$mailContent = "<h1>Send HTML Email using SMTP in PHP</h1>
+    <p>This is a test email I’m sending using SMTP mail server with PHPMailer.</p>";
+$mail->Body = $mailContent;
+
+if($mail->send()){
     echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}else{
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
