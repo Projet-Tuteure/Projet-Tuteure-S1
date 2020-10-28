bda600035fffbceb31a2194d1c7d4cef3f9290e7
teleyos <tristanduret@hotmail.fr>
2020-10-28 15:28:52 +0100

    changing contact form

--- contact/index.php
+++ contact/index.php
@@ -15,30 +15,27 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
-    $mail->Password   = 'secret';                               // SMTP password
+    $mail->Username   = '81528f312ff2d2';                     // SMTP username
+    $mail->Password   = '5035f518c6c1e5';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->setFrom('info@mailtrap.io', 'Mailtrap');
+    $mail->addAddress('recipient1@mailtrap.io', 'Tim');               // Name is optional
     $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->addCC('cc1@example.com', 'Elena');
+    $mail->addBCC('bcc1@example.com', 'Alex');
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Test Email via Mailtrap SMTP using PHPMailer';
+
+    $mailContent = "<h1>Send HTML Email using SMTP in PHP</h1>
+        <p>This is a test email I’m sending using SMTP mail server with PHPMailer.</p>";
+    $mail->Body = $mailContent;
 
     $mail->send();
     echo 'Message has been sent';
