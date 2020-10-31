4f758ef881cc716e3bbf0255b21687ca468d4276
Léo Carrez <71445843+Azmog25@users.noreply.github.com>
2020-10-31 15:25:23 +0100

    Add files via upload

---
+++ page_economie.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr" class="h-100">
+    <head>
+        <meta charset="UTF-8">
+        <title>L'économie</title>
+
+        <!-- Feuilles de style -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+
+    <!-- Body -->
+    <body class="h-100">
+        <div style="height: 300px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-1.41,106.08 C246.89,148.52 248.02,148.52 503.10,110.03 L500.00,0.00 L0.00,0.00 Z" style="stroke: none; fill: #000B76;"></path></svg></div>
+
+        <!-- barre de navigation -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+            <a class="text-white mr-3" href="#">Accueil</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">L'économie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">La technologie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contraintes Juridiques</a>
+                    </li>
+                </ul>
+                <a class="btn btn-outline-success" href="contact/index.php">Contact</a>
+            </div>
+        </nav>
+
+        <h1 class="w-auto mx-auto mb-5 text-white text-center" style="margin-top: -200px">L'aspect économique de la Bourse</h1>
+
+        <div class="card w-75 mx-auto mb-3">
+            <div class="card-body">
+                <h3 class="card-title">Titre de la partie</h3>
+                <h4 class="card-subtitle mb-2 text-muted">Titre sous partie</h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+        <div style="height: 250px; overflow: hidden; margin-top: -75px" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C177.48,63.64 301.63,33.05 513.26,-0.48 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #000B76;"></path></svg></div>
+
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
