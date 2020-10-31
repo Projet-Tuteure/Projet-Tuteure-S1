1ad993f044a65092223eca5255cbe1eaa65ae074
teleyos <tristanduret@hotmail.fr>
2020-10-31 15:07:28 +0100

    changement lien

--- header.php
+++ header.php
@@ -13,14 +13,14 @@
 
     <!-- barre de navigation -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top">
-      <a class="text-white mr-3" href="#">Accueil<span class="sr-only">(current)</span></a>
+      <a class="text-white mr-3" href="index.php">Accueil<span class="sr-only">(current)</span></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="index.php">L'économie</a>
+            <a class="nav-link" href="">L'économie</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">La technologie</a>
