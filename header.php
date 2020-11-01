f381547901f1af476404fd2a3e597ff3bfe7022f
Henry MONT <32devstudio@protonmail.com>
2020-11-01 13:16:49 +0100

    added responsiveness on mobile

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title>La Bourse</title>
 
+     <meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
+
     <!-- Feuilles de styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
