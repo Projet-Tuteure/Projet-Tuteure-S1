40c7a3723ed30448582248d1d287e80c0d74ddc0
teleyos <tristanduret@hotmail.fr>
2020-11-24 16:03:25 +0100

    aaaaah

--- pages/page_juridique.php
+++ pages/page_juridique.php
@@ -1,7 +1,8 @@
-<?php include "./pages/header.php"; ?>
+<?php include "header.php"; ?>
 
-    <h1 class="w-auto mx-auto mb-5 text-white text-center" style="margin-top: -200px">Les enjeux de la Bourse</h1>
+    <h1 class="w-auto mx-auto mb-5 text-white text-center" style="margin-top: -200px">Les contraintes juridiques</h1>
 
+    <!-- à copier-coller pour chaque partie -->
     <div class="card w-75 mx-auto mb-3 card_content">
       <div class="card-body">
         <h3 class="card-title">Titre</h3>
@@ -9,6 +10,7 @@
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
 
 <?php include "footer.php"; ?>
