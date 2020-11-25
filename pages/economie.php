b2cc735ee31a2e686e719cb12a66e4ad4834868b
Tristan Duret <56928284+teleyos@users.noreply.github.com>
2020-11-25 16:57:31 +0100

    Update economie.php

--- pages/economie.php
+++ pages/economie.php
@@ -3,13 +3,11 @@
 
         <div class="card w-75 mx-auto mb-3">
             <div class="card-body">
-                <h3 class="card-title">Titre de la partie</h3>
-                <h4 class="card-subtitle mb-2 text-muted">Titre sous partie</h4>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <h3 class="card-title">La Bourse: Pour qui? Pour quoi?</h3>
+                <h4 class="card-subtitle mb-2 text-muted">Le fondement de la bourse</h4>
+                <p class="card-text">Pour bien comprendre la bourse, il faut d'abord comprendre les marchés financiers. Les deux marchés principaux sont les marchés primaires et le secondaire. Le marché primaire désigne celui sur lequel un émetteur introduit des titres financiers (droit de propriété d'une entreprise ou d'un bien) pour la première fois. Le marché secondaire fait intervenir les acheteurs et les vendeurs de titres déjà présents sur le marché primaire. Comme exemple d'émetteur on peut trouver l'État et les entreprises. Quant aux acheteurs, ils peuvent être des États, des entreprises ou des particuliers. Les titres financiers sont donc des droits de propriété d'une entreprise ou d'un bien qui se manifeste en différents types.</p>
+                <p class="card-text">Les actions sont des titres de propriété d'une société émettrice qui sont vendus sur le marché. Plusieurs droits y sont associés, notamment le droit de participer aux assemblées générales de l'entreprise ou encore de percevoir une partie des bénéfices de celle-ci proportionnellement à l'investissement effectué. Les obligations (un autre type de titre de propriété) sont des prêts d'argent à une entreprise plutôt qu'un investissement. L'acheteur d'une obligation se verra verser annuellement un pourcentage de l'argent prêté (avec les intérêts).</p>
+                <p class="card-text">Au fil des années, des produits dérivés ont été créés. Ce sont des instruments financiers qui dépendent de la valeur de plusieurs titres. Par exemple, les options permettent de vendre ou d'acheter des titres à un prix prédéterminé. Les options sont un produit dérivé parmis beaucoup d'autres. </p>
             </div>
         </div>
 <?php include "footer.php"; ?>
