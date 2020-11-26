b793c30139f1c76de9bb70fe297fb1afb507029e
Léo Carrez <71445843+Azmog25@users.noreply.github.com>
2020-11-26 14:54:30 +0100

    Update juridique.php

--- pages/juridique.php
+++ pages/juridique.php
@@ -1,10 +1,10 @@
 <?php include "header.php"; ?>
 
-    <h1 class="w-auto mx-auto mb-5 text-white text-center" style="margin-top: -200px">Les contraintes juridiques</h1>
+<h1 class="w-auto mx-auto mb-5 text-white text-center" style="margin-top: -200px">Les contraintes juridiques</h1>
 
-    <!-- à copier-coller pour chaque partie -->
-    <div class="card w-75 mx-auto mb-3 card_content">
-      <div class="card-body">
+<!-- à copier-coller pour chaque partie -->
+<div class="card w-75 mx-auto mb-3 card_content">
+    <div class="card-body">
         <h3 class="card-title">Les lois régissants la bourse</h3>
         <h4 class="card-subtitle mb-2 text-muted">Délit d'initié</h4>
         <p class="card-text">Certains inverstisseurs en valeur immobilière se servent d'informations confidentielles pour augmenter leurs gains lors de transactions financières. Il se sépare en deux catégories :<br />
@@ -12,23 +12,31 @@
             - Le délit d'initié inirect : Dans ce cas, l'information est obtenue par un membre extérieur à l'entreprise.<br />
             Ce délit est puni par les autorités de contrôle des marchés et n'est pas à confondre avec l'analyse des informations divulguées publiquement.
         </p>
+        <div class="card" style="width:10rem;margin-left:80%;margin-top:-5%;">
+            <img src="../assets/AMF.png" class="card-img-top" alt="">
+        </div>
         <h4 class="card-subtitle mb-2 text-muted">Les entreprises</h4>
         <p class="card-text">Les entreprises se doivent de suivre une règle majeur lorsqu'elles sont cotées en bourse. On appelle cela une obligation de publication d'information. Elles doivent donc rendre publique certaines informations pour que les investisseurs puissent prendre la décision d'investir ou non. Les entreprises ne respectant pas cette loi sont alors sanctionnées par les autorités des marchés qui diffusent une liste des entreprises "retardataires", puis si les informations ne sont toujours pas diffusées, des poursuites judiciaires sont engagées pour obtenir une injonction de publication sous abstreinte.</p>
 
         <h3 class="card-title">Taxes et charges liées à la bourse</h3>
         <h4 class="card-subtitle mb-2 text-muted">Les placements d'ordres</h4>
         <p class="card-text">Le placement d'ordre implique des frais. Il en existe plusieurs. Les plus connus sont les frais de courtages qui sont soit une valeur fixe, soit le pourcentage d'un montant investi. Il existe néanmoins des frais moins connus comme la Taxe sur les Transactions Financières (TTF). Elle a pour but de freiner la spéculation et encourage l'investissement à long terme. Elle ne s'applique que sur les achats d'actions de société dont le siège social est situé en France et dont la capitalisation boursière est supérieur à 1 milliard d'euros.</p>
+        <div class="card" style="width:25rem;margin:auto;">
+            <img src="../assets/carnet_ordre.jpeg" class="card-img-top" alt="">
+        </div>
+        <br />
+
         <h4 class="card-subtitle mb-2 text-muted">Déclaration</h4>
         <p class="card-text">La cotisation subsidiaire maladie est une taxe apparue en 2017 qui vise les rentiers. Sa mise en place vise les personnes dont leurs revenus liés à l'activité professionnelle sont inférieur à 10% et dont les autres revenus sont supérieur à 25%. Les plus-values sont également soumises au prélèvement forfaitaire unique.</p>
 
         <h3 class="card-title">Les arnaques et pièges liées à la bourse</h3>
         <h4 class="card-subtitle mb-2 text-muted">Les CFD (Contract For Difference)</h4>
         <p class="card-text">Les CFD sont des produits dérivés de valeurs comme le CAC 40, le Dow Jones qui reproduisent les variations. Il est fonctionnel 24h/24h et fonctionne même quand la bourse est fermée et permet de trader. Il est malheureusement dangereux car il peut permettre de gagner de l'argent mais peut en faire perdre plus encore. Certaines sociétés de courtage vendent des CFD perdant qui sont basés sur des packages de valeurs peu prometteurs et ne mentionne pas les risques.<br />
-        La société IG Market à reçu une amende de 500.000€ en 2018 pour avoir vendu des CFD perdants à des clients.
+            La société IG Market à reçu une amende de 500.000€ en 2018 pour avoir vendu des CFD perdants à des clients.
         </p>
         <h4 class="card-subtitle mb-2 text-muted">Les options binaires</h4>
         <p class="card-text">Les options binaires visent les investisseurs en herbe. On appelle investisseur en herbe les investisseurs voulant faire beaucoup d'argent rapidement avec peu de capital. Ce genre de contrat peuvent néanmoins être intéressants pour les traders experimentés qui font du scalping (investissement sur de courtes échéances de quelques jours ou de quelques semaines) ou du day trading (investissement sur l'espace d'une journée). Ceux ci peuvent avoir besoin d'effet de levie car il misent sur les mouvements légers du marché. Leurs investissement sont néanmois calculés et reposent sur des analyses du marché.</p>
-      </div>
     </div>
+</div>
 
 <?php include "footer.php"; ?>
