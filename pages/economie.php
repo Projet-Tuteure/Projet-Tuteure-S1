601d165759ed393fd9adfb82aecfac5d7cbcd983
teleyos <tristanduret@hotmail.fr>
2020-11-26 17:32:26 +0100

    modification page économie

--- pages/economie.php
+++ pages/economie.php
@@ -3,12 +3,38 @@
 
         <div class="card w-75 mx-auto mb-3">
             <div class="card-body">
-                <h3 class="card-title">La Bourse: Pour qui? Pour quoi?</h3>
-                <h4 class="card-subtitle mb-2 text-muted">Le fondement de la bourse</h4>
-                <p class="card-text">Pour bien comprendre la bourse, il faut d'abord comprendre les marchés financiers. Les deux marchés principaux sont les marchés primaires et le secondaire. Le marché primaire désigne celui sur lequel un émetteur introduit des titres financiers (droit de propriété d'une entreprise ou d'un bien) pour la première fois. Le marché secondaire fait intervenir les acheteurs et les vendeurs de titres déjà présents sur le marché primaire. Comme exemple d'émetteur on peut trouver l'État et les entreprises. Quant aux acheteurs, ils peuvent être des États, des entreprises ou des particuliers. Les titres financiers sont donc des droits de propriété d'une entreprise ou d'un bien qui se manifeste en différents types.</p>
-                <p class="card-text">Les actions sont des titres de propriété d'une société émettrice qui sont vendus sur le marché. Plusieurs droits y sont associés, notamment le droit de participer aux assemblées générales de l'entreprise ou encore de percevoir une partie des bénéfices de celle-ci proportionnellement à l'investissement effectué. Les obligations (un autre type de titre de propriété) sont des prêts d'argent à une entreprise plutôt qu'un investissement. L'acheteur d'une obligation se verra verser annuellement un pourcentage de l'argent prêté (avec les intérêts).</p>
-                <p class="card-text">Au fil des années, des produits dérivés ont été créés. Ce sont des instruments financiers qui dépendent de la valeur de plusieurs titres. Par exemple, les options permettent de vendre ou d'acheter des titres à un prix prédéterminé. Les options sont un produit dérivé parmis beaucoup d'autres. </p>
+                <h3 class="card-title">Fonctionnement économique de la bourse</h3>
+                <h4 class="card-subtitle mb-2 text-muted">La Bourse: Pour qui? Pour quoi?</h4>
+		<p class="card-text">Il existe deux marchés: <strong>le marché primaire</strong> et <strong>le marché secondaire</strong>.</p>
+		<li>Le <strong>marché primaire</strong> désigne le marché sur lequel on introduit des titres de propriété pour la première fois.</li>
+		<li>Le <strong>marché secondare</strong> désigne celui sur lequel les acheteurs et les vendeurs de titres déjà acquis.</li><br>
+		<p class="card-text">La bourse est donc composée d'émetteurs et d'acheteurs. Les émetteurs principaux sont l'État et les entreprises tandis que les acheteurs principaux sont les sont l'État, les entreprises et les particuliers.</p>
+		<h4 class="card-subtitle mb-2 text-muted">Les types de titre</h4>
+		<li>Les <strong>actions</strong> sont les titres les plus connues. Elles représentent une part de la société émetrice: le propriétaire de cette action peut donc obtenir un revenu proportionnel à l'investissement (<strong>les dividendes</strong>).</li>
+		<li>Les <strong>obligations</strong> représentent un près à une entreprise. L'entreprise émettrice rendra plus que l'argent emprunté tout les ans.</li>
+		<li>Depuis plusieurs années on voit apparaître des <strong>produits dérivés</strong>. Ce sont des indicateurs financiers dont la valeurs dépends de ses <strong>sous-jacents</strong> (les titres qui composent l'indicateur).</li><br>
+                <h3 class="card-title">Les investisseurs historiques</h3>
+		<h4 class="card-subtitle mb-2 text-muted">Lehman Brothers</h4>
+		<p><strong>Lehman Brothers Holdings Inc.</strong> était la quatrième plus grosse banque d'investissement avant de faire faillite en 2008 (crise des <strong>sub-primes</strong>). Elle était un acteur majeur du marché des emprunts d'Etat americains. </p>
+		<h4 class="card-subtitle mb-2 text-muted">Les fonds d'investissements</h4>
+		<p>Ce sont des sociétés qui investissent l'argent de particuliers en bourse et leurs donnent les dividendes en prenant une part de ceux-ci. Il existe plusieurs type de fonds d'investissements dont les <strong>Organisme de Placement Collectif en Valeurs Mobilière</strong> (OPCVM) qui se spécialisent dans les investissements, comme indiqué dans le nom, immobiliers.</p>
+		<h3 class="card-title">La démocratisation de la bourse</h3>
+		<p>Depuis maintenant plusieurs années, on constate une démocratisation de la bourse. Pourquoi?</p>
+		<h4 class="card-subtitle mb-2 text-muted">Fort potentiel spéculatif</h4>
+		<p>La raison évidente est que la bourse peut permettre de gagner beaucoup d'argent en un faible laps de temps. On pourrait presque la comparer à un casino par son côté imprévisible.</p>
+		<h4 class="card-subtitle mb-2 text-muted">Les courtiers en ligne</h4>
+		<p>On peut aussi facilement imaginer que l'apparition de nombreuses plateforme de courtage en ligne encourage le grand public à investir puisqu'elles s'y adressent directement. </p>
+		<h3 class="card-title">L'impact économique de la bourse</h3>
+		<p>La bourse a un impact certain sur l'économie.</p>
+		<li>Elle permet aux entreprises d'avoir des investisseurs sans être dépendant d'une seul entité.</li>
+		<li>La bourse montre la santé économique d'un pays: Un pays avec une économie en mauvaise santé verra les cours de ses cotation en bourse dégringoler.</li>
+		<h3 class="card-title">De l'investissement à la spéculation</h3>
+		<h4 class="card-subtitle mb-2 text-muted">L'apparition de bulles spéculative</h4>
+		<p>Une <strong>bulle spéculative</strong> est un phénomène économique qui désigne un investissement collectif et démesuré et souvent irrationel en un produit, faisant augmenter son cours considérablement. Un bon exemple de bulle spéculative est le bitcoin puisqu'en moins d'un ans la valeur du bitcoin est passé de 3 centimes à environ 19 000 euros avant de s'écrouler: On parle d'explosion de la bulle.</p>
+		<p>On voit donc avec l'apparition de bulle spéculative, une apparition d'investissements pharamineux juste avant une explosion de bulle. On peut par exemple citer le cas de Jérôme Keriviel, un investisseur de la société générale. Il leur à fait perdre près de 4 milliard d'euros en 2008 en faisant des investissement risqués.</p>
+		
+		
+		
             </div>
-                <h4 class="card-subtitle mb-2 text-muted">Investisseurs historiques</h4>
         </div>
 <?php include "footer.php"; ?>
