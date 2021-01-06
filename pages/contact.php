c8114fadbe085fb4cb3e2a3d909770305556c300
Henry Mont <61246587+32devstudio@users.noreply.github.com>
2021-01-06 10:50:55 +0100

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -67,7 +67,7 @@ if($form_filed){
           </div>
           <div class="form-group">
             <label>Corps</label>
-            <textarea class="form-control" placeholder="Corps du message" name="mailbody" required="" rows="5"></textarea>
+            <textarea class="form-control" placeholder="Corps du message" name="mailbody" required="" rows="10"></textarea>
           </div>
           <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
